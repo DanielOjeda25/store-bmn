706a8254950e8c6f5ee2b3fcae611f473dd9c62e
DanielOjeda25 <ojedadanielalejandro333@gmail.com>
2022-12-07 12:14:25 -0300

    cart items logic

--- pages/product/[slug].jsx
+++ pages/product/[slug].jsx
@@ -1,17 +1,29 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useContext } from 'react'
 import Layout from '../../components/Layout'
 import data from '../../utils/data'
+import { Store } from '../../utils/Storage'
 
 const ProductScreen = () => {
+  const { state, dispatch } = useContext(Store)
   const { query } = useRouter()
   const { slug } = query
 
   const product = data.products.find((x) => x.slug === slug)
   if (!product) return <div>Product Not found</div>
 
+  const addToCardHandler = () => {
+    const existItem = state.cart.cartItems.find((x) => x.slug === product.slug)
+    const quantity = existItem ? existItem.quantity + 1 : 1
+    if(product.countInStock < quantity) {
+      alert('Producto fuera de stock')
+      return
+    }
+
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity} })
+  }
   return (
     <Layout title={product.name}>
       <div className='py-2'>
@@ -50,7 +62,12 @@ const ProductScreen = () => {
               <div>Status</div>
               <div>{product.countInStock > 0 ? 'In stock' : 'Unvailable'}</div>
             </div>
-            <button className='primary-button w-full'>Add to cart</button>
+            <button
+              className='primary-button w-full'
+              onClick={addToCardHandler}
+            >
+              Add to cart
+            </button>
           </div>
         </div>
       </div>
